b1c9aa3d45596b83140657551951533e81f6efcf
Andrew Prine <andrewpprine@gmail.com>
2018-06-28 23:10:18 -0700

    trying to get timer going

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,34 @@
+// start a timer
+// show a question and a list of radio buttons
+// record player's answer
+// show next question and buttons, record answer, etc
+// when all 10 questions answered OR when timer is up, show all 10 questions with both correct answer and player answer
+// button to play again at bottom of results page
+
 var seconds = 120;
 
-function timer(){
-   seconds--;
-   $("#timer").text(seconds);
-   if (seconds === 0){
-     alert("Time's Up!")
-   }
+function countdown(){
+   timer = setInterval(twoMinutes, 1000);
+   function twoMinutes(){
+     if (seconds === 0){
+       alert("You're out of time!");
+       gameover();
+     }else if (seconds > 0){
+       seconds--;
+     }
+    $("#timer").text(seconds);
+    seconds--;
+  }
  }
-$("button").click(function(){
-   alert('GO!');
-   $('#timer').text(seconds);
 
-});
+
+
+
+
+$("button").click(function(){
+   $('#preGameplay').hide();
+   $('#gameplay').show();
+   $('#timer').text(seconds);
+   console.log(seconds);
+   seconds--;
+   });
