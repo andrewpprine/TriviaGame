522f6821d4addda71fdc7a584e4ecf67fda80b77
Andrew Prine <andrewpprine@gmail.com>
2018-06-29 07:17:17 -0700

    timer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,33 +4,32 @@
 // show next question and buttons, record answer, etc
 // when all 10 questions answered OR when timer is up, show all 10 questions with both correct answer and player answer
 // button to play again at bottom of results page
-var timer;
-var seconds = 120;
+var seconds = 5;
 
 function countdown(){
-   twoMinutes = setInterval(timer, 1000);
-   function timer(){
-     if (seconds === 0){
-       alert("You're out of time!");
-       gameover();
-     }else if (seconds > 0){
-       seconds--;
-       console.log(seconds);
-     }
+  timer = setInterval(timer, 1000);
+  function timer(){
+    if (seconds == 0){
+      clearInterval(timer);
+      alert("You're out of time!");
+      gameover();
+    }else if (seconds > 0){
+      seconds--;
+      console.log(seconds);
+    }
     $("#timer").text(seconds);
   }
  }
 
  function gameover(){
-  clearInterval(timer);
   $('#gameplay').hide();
   $('#results').show();
 }
 
 $("button").click(function(){
-   $('#preGameplay').hide();
-   $('#gameplay').show();
-   $('#timer').text(seconds);
-   countdown();
-  });
+  $('#preGameplay').hide();
+  $('#gameplay').show();
+  $('#timer').text(seconds);
+  countdown();
+});
 
